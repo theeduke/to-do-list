3f987e4cde9e41b4a9eadf9e9db53b86d60568ba
ManNjoro <elijohnmwoho@gmail.com>
2022-07-02 23:13:08 +0300

    first commit

--- assets/main.js
+++ assets/main.js
@@ -95,10 +95,22 @@ const addNewTodo = () => {
 }
 
 const deleteTodo = (itemId) => {
+	Todos = Todos.filter(todo => todo.id != itemId);
+	displayAllTodos();
 	axios.delete(`http://localhost:8000/posts/${itemId}`);
 };
 
 const updateTodo = () => {
+	const todos = Todos.map(todo=>{
+		if(todo.id === idField.value){
+			todo.status = "Not complete";
+			todo.body = bodyField.value;
+			todo.timestamp = timeField.value;
+			return todo;
+		}else{
+			return todo;
+		}
+	})
 	const id = idField.value;
 	const timestamp = timeField.value;
 	const body = bodyField.value;
@@ -114,6 +126,17 @@ const updateTodo = () => {
 };
 
 const markTodoAsComplete = (itemId) => {
+	
+	const todos = Todos.map(todo=>{
+		if(todo.id === itemId){
+			todo.status = "Complete";
+			return todo;
+		}else{
+			return todo;
+		}
+	})
+	Todos = todos;
+	displayAllTodos();
 	axios.patch(`http://localhost:8000/posts/${itemId}`, { status: "Complete" });
 };
 
